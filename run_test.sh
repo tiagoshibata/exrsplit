4b96477d686506be6c00401ed624f686b3856cc0
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2016-10-11 16:43:56 -0300

    Check pytest-xdist validity on each Python version

--- run_test.sh
+++ run_test.sh
@@ -2,15 +2,17 @@
 set -e
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-if python -m pytest --fixtures | grep 'xdist\.plugin' > /dev/null ; then
-    PYTEST_OPTS='-n auto'
-else
-    echo "pytest-xdist not detected, tests won't be run in parellel"
-fi
 
 for PYTHON in python2 python3 ; do
     echo ------------------------------------------------
     echo Testing with $PYTHON...
+
+    if $PYTHON -m pytest --fixtures | grep 'xdist\.plugin' > /dev/null ; then
+        PYTEST_OPTS='-n auto'
+    else
+        echo "pytest-xdist not detected, tests won't be run in parellel"
+    fi
+
     if [[ $PYTEST_OPTS ]] ; then
         $PYTHON "$SCRIPT_DIR/setup.py" test --addopts "$PYTEST_OPTS"
     else
